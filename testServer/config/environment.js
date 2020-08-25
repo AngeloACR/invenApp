475a0b125acf27a08ece1eabd16cb43ce44e4a73
Angelo Crincoli <angelocrincoli91@gmail.com>
2020-08-24 21:46:10 -0400

    Corrigiendo db de prueba

--- testServer/config/environment.js
+++ testServer/config/environment.js
@@ -1,8 +1,8 @@
 module.exports = {
 
 	// DATABASES ADDRESSES
-	prodDB: "mongodb://localhost:27017/inven",
-	testDB: "mongodb://localhost:27017/inven",
+	prodDB: "mongodb://localhost:27017/inventest",
+	testDB: "mongodb://localhost:27017/inventest",
 
 	// SECRETS FOR ENCRYPTION
 	authSecret: 'Shall I let you pass, or Shall I not?',
